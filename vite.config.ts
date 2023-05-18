bd6e0b786d659ebcec339781adef81970fc798db
Dante Durant <dantedurant@Dantes-MacBook-Air.local>
2023-05-18 17:38:07 +0200

    netlify fix7

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		assetsDir: './src/lib/images', // Change 'assets' to the actual directory name where Logo.png is located
+	},
 });
