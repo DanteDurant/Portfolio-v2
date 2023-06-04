b5c9f036d2e14059b93b7a0ac029964999715594
Dante Durant <dantedurant@Dantes-MacBook-Air.local>
2023-06-04 07:57:40 +0200

    responsive test1

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,27 @@
+// import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import sass from 'sass';
-import fs from 'fs';
 
-const globalStyles = fs.readFileSync('./src/global.scss', 'utf-8');
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+	scss: {
+
+		implementation: sass,
+	},
+
 	kit: {
+
 		adapter: adapter(),
+
 		prerender: {
 			handleMissingId: "ignore",
 		},
 	},
-	preprocess: vitePreprocess({
-		scss: {
-			implementation: sass,
-			prependData: globalStyles,
-		},
-	}),
+
 };
 
 export default config;
