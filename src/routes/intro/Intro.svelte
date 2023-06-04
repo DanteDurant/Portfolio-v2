9c0567270382067009910b437327192dc3cf197a
Dante Durant <dantedurant@Dantes-MacBook-Air.local>
2023-06-04 07:24:55 +0200

    scrolling issues resolved, some responsive design play11

--- src/routes/intro/Intro.svelte
+++ src/routes/intro/Intro.svelte
@@ -5,7 +5,7 @@
   import ArrowDown from "$lib/images/logos/ArrowDown.svelte";
   import sharedStyles from "../../utility/sharedStyles.scss";
   import keyFrames from "../../utility/keyFrames.css";
-  import mediaQueries from "../../utility/mediaQueries.scss";
+  // import mediaQueries from "../../utility/mediaQueries.scss";
 
   let subVisible = false;
   let buttonVisible = false;
