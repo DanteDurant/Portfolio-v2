a4c4460a9a0769d57beeecfbedf0fd994da2591f
DanteDurant <dantecodedurant@gmail.com>
2023-08-21 11:09:05 +0200

    new build

--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -2,6 +2,6 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.cfd955a2.js","_app/immutable/chunks/index.c9343b21.js","_app/immutable/chunks/singletons.27683f87.js","_app/immutable/chunks/index.6c6c6af6.js"];
+export const imports = ["_app/immutable/nodes/1.61569bd5.js","_app/immutable/chunks/index.c9343b21.js","_app/immutable/chunks/singletons.efda8ac8.js","_app/immutable/chunks/index.6c6c6af6.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .netlify/server/nodes/7.js
+++ .netlify/server/nodes/7.js
@@ -2,6 +2,6 @@
 
 export const index = 7;
 export const component = async () => (await import('../entries/pages/work/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/7.de773c50.js","_app/immutable/chunks/index.c9343b21.js","_app/immutable/chunks/Work.9f56ab1c.js","_app/immutable/chunks/store.9bcf751c.js","_app/immutable/chunks/index.6c6c6af6.js","_app/immutable/chunks/Bounce.dec36ece.js","_app/immutable/chunks/Button.d796d733.js","_app/immutable/chunks/BounceText.40be5953.js"];
+export const imports = ["_app/immutable/nodes/7.a247d07a.js","_app/immutable/chunks/index.c9343b21.js","_app/immutable/chunks/Work.c98d4832.js","_app/immutable/chunks/store.9bcf751c.js","_app/immutable/chunks/index.6c6c6af6.js","_app/immutable/chunks/Bounce.dec36ece.js","_app/immutable/chunks/Button.d796d733.js","_app/immutable/chunks/BounceText.40be5953.js"];
 export const stylesheets = ["_app/immutable/assets/Work.4d3ca055.css","_app/immutable/assets/Bounce.21e0c21b.css","_app/immutable/assets/Button.11228949.css","_app/immutable/assets/BounceText.bbcccff1.css"];
 export const fonts = [];
